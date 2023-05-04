16afa649385c02d3e508301d73d209ce0e52e5bd
Menna <menna.Elgallad@baianat.com>
2023-05-04 17:20:55 +0300

    all added

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,6 +5,7 @@ const colors = ref();
 const loading = ref(true);
 const countedTypes = ref();
 const router = useRouter();
+
 onMounted(() => {
   const token = localStorage.getItem("token");
   if (token) {
@@ -65,18 +66,26 @@ async function getDataCars() {
 }
 getDataCars();
 const chartData = ref({
-  labels: ["Q1", "Q2", "Q3", "Q4"],
+  labels: ["Sut", "Sun", "Mon", "Tue", "wed", "Thu", "Fri"],
   datasets: [
     {
-      label: "Sales",
-      data: [540, 325, 702, 620],
+      label: "Entry Count in days",
+      data: [200, 300, 311, 351, 310, 312, 30],
       backgroundColor: [
+        "rgba(255, 159, 64, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
         "rgba(255, 159, 64, 0.2)",
         "rgba(75, 192, 192, 0.2)",
         "rgba(54, 162, 235, 0.2)",
         "rgba(153, 102, 255, 0.2)",
       ],
       borderColor: [
+        "rgb(255, 159, 64)",
+        "rgb(75, 192, 192)",
+        "rgb(54, 162, 235)",
+        "rgb(153, 102, 255)",
         "rgb(255, 159, 64)",
         "rgb(75, 192, 192)",
         "rgb(54, 162, 235)",
@@ -97,86 +106,99 @@ const chartOptions = ref({
 
 <template>
   <div class="grid">
-    <div class="col-12 lg:col-6 xl:col-5 card">
+    <div class="col-12 card">
       <div class="mb-0">
         <div class="mb-3">
           <div class="part1 flex">
-            <div class="ml-3 flex justify-content-between w-full">
-              <span class="block text-900 text-2xl font-semibold"
+            <div class="ml-3 head">
+              <span class="block text-900 text-xl font-semibold p-2"
                 >Cars in System</span
               >
-              <div class="text-900 text-2xl font-semibold">
-                {{ cardata?.length }}
-              </div>
             </div>
           </div>
-          <div class="part2 grid mt-4" v-if="countedTypes">
+          <div
+            class="part2 flex w-full justify-content-around mt-4"
+            v-if="countedTypes"
+          >
             <div
-              class="type col-6 flex flex-column align-items-center"
+              class="type flex flex-column align-items-center"
               v-for="(item, index) in countedTypes"
               :key="index"
             >
               <div
+                v-if="item[0] === 'ملاكي'"
                 class="flex align-items-center justify-content-center bg-blue-100 border-round"
                 style="width: 3.5rem; height: 3.5rem"
               >
                 <!-- <i class="pi pi-car text-blue-500 text-5xl" /> -->
                 <Icon
-                  v-if="item[0] === 'ملاكي'"
                   name="ic:baseline-directions-car-filled"
+                  class="text-3xl text-blue-500"
                 ></Icon>
               </div>
 
-                <div
+              <div
                 v-if="item[0] === 'نقل'"
-                class="flex align-items-center justify-content-center bg-blue-100 border-round"
+                class="flex align-items-center justify-content-center bg-pink-100 border-round"
                 style="width: 3.5rem; height: 3.5rem"
               >
-              <Icon  name="fe:truck" class=" text-pink-400"></Icon>
-
-                </div>
-                <div
+                <Icon name="fe:truck" class="text-pink-500 text-3xl"></Icon>
+              </div>
+              <div
                 v-if="item[0] === 'أتوبيس'"
-                class="flex align-items-center justify-content-center bg-blue-100 border-round"
-                style="width: 3.5rem; height: 3.5rem"
-              >
-              <Icon
-
-                  name="ic:baseline-directions-bus"
-                ></Icon>
-                </div>
-                <div
-                v-if="item[0] === 'أجنبية'"
-                class="flex align-items-center justify-content-center bg-blue-100 border-round"
+                class="flex align-items-center justify-content-center bg-orange-100 border-round"
                 style="width: 3.5rem; height: 3.5rem"
               >
                 <Icon
-
-                  name="ic:twotone-taxi-alert"
+                  name="ic:baseline-directions-bus"
+                  class="text-3xl text-orange-500"
                 ></Icon>
-                </div>
-                <div
-                v-if="item[0] === 'تاكسي'"
-                class="flex align-items-center justify-content-center bg-blue-100 border-round"
+              </div>
+              <div
+                v-if="item[0] === 'أجنبية'"
+                class="flex align-items-center justify-content-center bg-purple-100 border-round"
                 style="width: 3.5rem; height: 3.5rem"
               >
-
-                <Icon name="map:taxi-stand"></Icon>
-                </div>
+                <Icon
+                  name="ic:twotone-taxi-alert"
+                  class="text-3xl text-purple-500"
+                ></Icon>
               </div>
-              <div class="car-data">
-                <div class="text-2xl font-bold ml-2 mt-2">{{ item[1] }}</div>
-                <div class="text-900 font-medium mr-2">{{ item[0] }}</div>
+              <div
+                v-if="item[0] === 'تاكسي'"
+                class="flex align-items-center justify-content-center bg-cyan-100 border-round"
+                style="width: 3.5rem; height: 3.5rem"
+              >
+                <Icon
+                  name="map:taxi-stand"
+                  class="text-3xl text-cyan-500"
+                ></Icon>
+              </div>
+
+              <div
+                class="car-data mt-2 flex flex-column justify-content-center align-items-center"
+              >
+                <h2 class="text-2xl font-bold">{{ item[1] }}</h2>
+                <p class="text-gray-500 font-medium">{{ item[0] }}</p>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-
-    <div class="col-12 xl:col-7">
+    <div class="col-12 xl:col-6">
       <div class="card">
-        <Chart type="bar" :data="chartData" :options="chartOptions" />
+        <div class="ml-3 head">
+          <span class="block text-900 text-xl font-semibold p-2"
+            >Entry history conut</span
+          >
+        </div>
+        <Chart
+          type="bar"
+          :data="chartData"
+          :options="chartOptions"
+          class="mt-3"
+        />
       </div>
     </div>
     <div class="col-12 xl:col-6">
@@ -198,9 +220,10 @@ const chartOptions = ref({
   transition: all 0.5s ease;
   overflow: hidden;
   width: 100%;
-  height: 100%;
+  height: 80%;
   border-radius: 1.5rem;
   display: block;
+  margin-top: 2rem;
   &:hover {
     box-shadow: 0 0 8px 2px #cccccc9a;
   }
@@ -209,5 +232,12 @@ const chartOptions = ref({
   width: 100%;
   height: 100%;
   object-fit: cover;
+}
+.card {
+  height: 100%;
+}
+.type {
+  padding: 0.5rem 1rem;
+  border-bottom: 2px solid #cccccc98;
 }
 </style>
